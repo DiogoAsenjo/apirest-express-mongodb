0ae3c02dc067e270fe2751a60575204ffd0cfa86
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-18 15:45:27 -0300

    Criando as primeiras rotas com http

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const port = 3333;
 
+const rotas = {
+    '/': 'Servidor funcionando normalmente!',
+    '/nodeJS': 'Diogo, Ellen, Murilo, Neemias e Pedro',
+    '/techlead': 'Issac monstro'
+}
+
 const server = http.createServer((requisição, resposta) => {
     resposta.writeHead(200, {'Content-Type': 'text/plain'});
-    resposta.end('Servidor funcionando normalmente!');
+    resposta.end(rotas[requisição.url]);
 })
 
 server.listen(port, () => {
