f0668140afb72927bb8175d757f17b001786120b
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-21 09:43:35 -0300

    Criando a primeira rota post - criação de usuário

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import app from './src/app.js'
 
 const port = process.env.PORT || 3333; //É boa prática pra ir se acostumando já colocar o process.env, pois será o futuro ambiente de produção. 
 
-
 app.listen(port, () => {
     console.log(`Servidor rodando na porta http://localhost:${port}`);
 })
